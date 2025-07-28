02a0a8455a7024c06a18c20ff8468c43d232e0bb
Abdulrahmanmoin <iam.armoin@gmail.com>
2025-07-28 20:37:34 +0500

    Remove the unnessary import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
-import Home2 from "@/components/Home2";
 import HomeComp from "@/components/HomeComp";
 import Projects from "@/components/Projects";
 import Services from "@/components/Services";
@@ -10,7 +9,6 @@ export default function Home() {
   return (
     <>
       <HomeComp />
-      {/* <Home2 /> */}
       <About />
       <Services />
       <Projects />
